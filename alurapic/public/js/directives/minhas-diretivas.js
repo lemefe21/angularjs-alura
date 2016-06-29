9f48039df1e651433a47407a55b1096c599fd6ef
Felipe Leme <lemefe21@gmail.com>
2016-06-29 19:48:16 -0300

    Criando nossas próprias diretivas, utilizando transclude para indicar a crição de elementos filhos e separando templates do js das diretivas

--- alurapic/public/js/directives/minhas-diretivas.js
+++ alurapic/public/js/directives/minhas-diretivas.js
@@ -10,13 +10,23 @@ angular.module('minhasDiretivas', [])
     //C - comment
 
     //escopo isolado/privado - principio da reutilização
-    //$scope é do controller esse scope é da diretiva
+    //$scope é do controller esse 'scope' é da diretiva
     ddo.scope = {
-      //@titulo, ou quando o nome da interface for igual a propriedade da diretiva >> '@'
-      titulo : '@'
-    }
+      //@titulo, ou quando o nome da interface for igual ao atributo da diretiva >> '@'
+      //'@' indica que titulo vai receber o valor passado pelo atributo da interface de comunicação
+      //como string
+      titulo : '@' //imagen001 ou imagen002
+    };
 
+    //manter o elemento filho(imagem) do html onde o angular manipula o DOM
+    //no ddo.template
+    ddo.transclude = true;
 
+    //e na marcação do template informa na tag que recebe o elemento filho
+    //ng-transclude, dentro de meu-painel.html utilizando templateUrl
+
+    //separação do template
+    ddo.templateUrl = 'js/directives/meu-painel.html';
 
     //directive difinition object - DDO
     return ddo;
@@ -29,7 +39,10 @@ angular.module('minhasDiretivas', [])
 //</div>
 
 //restrict >> A
-//<meu-painel titulo="">  //titulo >> interface de comunicação com a diretiva
-                          //maneira como é passado o dado para dentro do escopo
-                          //privado da diretiva
+//<meu-painel titulo="imagen001"> //titulo >> interface de comunicação com a diretiva
+                                //maneira como é passado o dado para dentro do escopo
+                                //privado da diretiva
+//</meu-painel>
+//ou
+//<meu-painel titulo="imagen002">
 //</meu-painel>
